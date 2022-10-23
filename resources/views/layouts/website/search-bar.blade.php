aabc5d5e9541214b2fe685fc292fc88b1b0537a2
Kareem-Tarek <kareemtarekpk@gmail.com>
2022-10-23 07:23:49 +0200

    ...

--- resources/views/layouts/website/search-bar.blade.php
+++ resources/views/layouts/website/search-bar.blade.php
@@ -135,5 +135,7 @@ Selector_Z {text-align: var(--all_center_aligns);}
 .dropdown a, .dropdown .dropdown-menu li a {color: var(--black_color);}
 
 .dropdown .dropdown-menu li a:hover {color: #0083FF; transition: all 0.30s ease-in-out;}
+
+.dropdown .dropdown-menu li a:not(:hover) {color: #000000; transition: all 0.30s ease-in-out;}
 </style>
 
