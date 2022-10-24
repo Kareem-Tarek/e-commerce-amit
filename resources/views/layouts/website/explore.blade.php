4ff463dd0e268b455d63d0103e71f4a29a1ca5b2
Kareem-Tarek <kareemtarekpk@gmail.com>
2022-10-24 11:18:50 +0200

    ...

---
+++ resources/views/layouts/website/explore.blade.php
@@ -0,0 +1,56 @@
+<!-- ***** Explore Area Starts ***** -->
+<section class="section" id="explore">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="left-content">
+                    <h2>Explore Our Products</h2>
+                    <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique quasi ipsum, obcaecati eum quaerat error magni dolore ipsa distinctio commodi quae earum minima exercitationem veniam iste rerum praesentium eos? Commodi.</span>
+                    <div class="quote">
+                        <i class="fa fa-quote-left"></i><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit non quas ullam neque, por.</p>
+                    </div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste optio at obcaecati laudantium, minus saepe accusantium quia provident, exercitationem soluta quidem quo voluptate.</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo <a href="javascript:void(0);" style="color: rgb(46, 46, 222):">molestias velit</a>, doloribus totam labore enim distinctio nemo, atque laudantium sint sunt minus in.</p>
+                    <div class="main-border-button">
+                        <a href="{{ route('products') }}">Discover More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="right-content">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="leather">
+                                <h4>
+                                    @foreach($latest_product as $latest_product_result)
+                                        <a href="{{ route('single_product_page' , $latest_product_result->id) }}" style="color:inherit; transition: all 0.25s ease-in-out;" onMouseOver="this.style.color='#3385ff'" onMouseOut="this.style.color='inherit'">
+                                            {{ $latest_product_result->name ?? 'No product found!' }}
+                                        </a>
+                                    @endforeach
+                                </h4>
+                                <span>Checkout Latest Items</span>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="first-image">
+                                <img src="assets/images/explore-image-01.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="second-image">
+                                <img src="assets/images/explore-image-02.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="types">
+                                <h4>Different Types</h4>
+                                <span>Over {{\App\Models\Product::count()-5}} Products</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ***** Explore Area Ends ***** -->
--- resources/views/website/website/home.blade.php
+++ resources/views/website/website/home.blade.php
@@ -302,62 +302,7 @@
 </div>
 <!-- Product Items Ends -->
 
-<!-- ***** Explore Area Starts ***** -->
-<section class="section" id="explore">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6">
-                <div class="left-content">
-                    <h2>Explore Our Products</h2>
-                    <span>You are allowed to use this AA HTML CSS template. You can feel free to modify or edit this layout. You can convert this template as any kind of ecommerce CMS theme as you wish.</span>
-                    <div class="quote">
-                        <i class="fa fa-quote-left"></i><p>You are not allowed to redistribute this template ZIP file on any other website.</p>
-                    </div>
-                    <p>There are 5 pages included in this AA Template and we are providing it to you for absolutely free of charge at our TemplateMo website. There are web development costs for us.</p>
-                    <p>If this template is beneficial for your website or business, please kindly <a rel="nofollow" href="https://paypal.me/templatemo" target="_blank">support us</a> a little via PayPal. Please also tell your friends about our great website. Thank you.</p>
-                    <div class="main-border-button">
-                        <a href="{{ route('products') }}">Discover More</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6">
-                <div class="right-content">
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <div class="leather">
-                                <h4>
-                                    @foreach($latest_product as $latest_product_result)
-                                        <a href="{{ route('single_product_page' , $latest_product_result->id) }}" style="color:inherit; transition: all 0.25s ease-in-out;" onMouseOver="this.style.color='#3385ff'" onMouseOut="this.style.color='inherit'">
-                                            {{ $latest_product_result->name ?? 'No product found!' }}
-                                        </a>
-                                    @endforeach
-                                </h4>
-                                <span>Checkout Latest Items</span>
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="first-image">
-                                <img src="assets/images/explore-image-01.jpg" alt="">
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="second-image">
-                                <img src="assets/images/explore-image-02.jpg" alt="">
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="types">
-                                <h4>Different Types</h4>
-                                <span>Over {{\App\Models\Product::count()-5}} Products</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-<!-- ***** Explore Area Ends ***** -->
+@include('layouts.website.explore')
 
 @include('layouts.website.subscription-and-contact-info')
 
