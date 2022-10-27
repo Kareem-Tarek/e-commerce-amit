f17e9bc2d448dc8c6174e1bd822845b7145ca750
Kareem-Tarek <kareemtarekpk@gmail.com>
2022-10-28 01:27:03 +0200

    ...

--- resources/views/website/products/items-discounts/1percent_10percent.blade.php
+++ resources/views/website/products/items-discounts/1percent_10percent.blade.php
@@ -129,11 +129,11 @@
                             </div>
                         @endif
                     @endauth
-
+                    
                     @if(Auth::guest())
                         <div style="margin-top: 2%; margin-bottom: 3%;">
-                            <a href="{{ route('cart-unregistered') }}"><input class="btn btn-primary" type="submit" value="Add to cart" name="" style="padding: 1.5% 3%; border-radius: 4px;"></a>
-                            <a href="{{ route('favorites-unregistered') }}"><input class="btn btn-success" type="submit" value="Add to favorites" name="" style="padding: 1.5% 3%; border-radius: 4px;"></a>
+                            <a class="add-to-cart-btn" href="{{ route('cart-unregistered') }}" name="">Add To Cart</a>
+                            <a class="add-to-favorites-btn" href="{{ route('favorites-unregistered') }}">Add To Favorites</a>
                         </div>
                     @endif
             </div>
