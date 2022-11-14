d8232539a0b0559f389f01278fea61e2be87a753
Kareem-Tarek <kareemtarekpk@gmail.com>
2022-11-14 10:50:27 +0200

    ....

--- resources/views/layouts/website/admin-product-control-website.blade.php
+++ resources/views/layouts/website/admin-product-control-website.blade.php
@@ -5,7 +5,6 @@
     </a> --}}
 
     <div class="input-group">
-
         <!---- By using "ancor" will take the admin to the edit form for the current product ---->
             {{-- <a href="{{route('products.edit',$product->id)}}" class="" style="background-color: rgb(36, 157, 36); color: snow; border-radius: 4px;">
                 <i class="fa-solid fa-pen-to-square"></i> Edit
@@ -13,7 +12,7 @@
         <!--------------------------------------------------------------------------------------->
 
         <!----------- By using "button" will take the admin to the edit form for the current product ---------->
-            <button type="button" class="form-control btn btn-success" onclick="window.location='{{ route('products.edit',[$product->id]) }}'"><i class="fa-solid fa-pen-to-square"></i> Edit</button>
+            <button type="button" class="form-control edit-btn" onclick="window.location='{{ route('products.edit',[$product->id]) }}'"><i class="fa-solid fa-pen-to-square"></i> Edit</button>
         <!----------------------------------------------------------------------------------------------------->
 
         <span class="input-group-btn" style="width: 5px;"></span> <!-- reducong the gap between them to zero -->
@@ -22,7 +21,8 @@
             'route' => ['products.destroy',$product->id],
             'method' => 'delete'
         ])!!}
-        <button class="form-control btn btn-danger" type="submit" onclick="return confirm('Are you sure that you want to delete - {{$product->name}}?')"><i class="fa-solid fa-trash"></i> Delete</button>
+        <button class="form-control delete-btn" type="submit" onclick="return confirm('Are you sure that you want to delete - {{$product->name}}?')"><i class="fa-solid fa-trash"></i> Delete</button>
+        {!! Form::close() !!}
     </div>
     
     {{-- <button class="btn btn-danger btn-xs" onclick="return confirm('Are you sure that you want to delete - {{$product->name}}?')" type="submit" title="{{'Delete'." ($product->name)"}}"><i class="fa-solid fa-trash"></i> Delete </button> --}}
