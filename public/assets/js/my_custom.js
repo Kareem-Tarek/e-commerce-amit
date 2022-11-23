726ecc4361e8ac8f5031cc043d8083d6202f7c21
Kareem-Tarek <kareemtarekpk@gmail.com>
2022-11-23 02:21:43 +0200

    ....

--- public/assets/js/my_custom.js
+++ public/assets/js/my_custom.js
@@ -25,46 +25,46 @@ function show_hide_password_function_login(){
  
  /********************************* START show/hide password dot-eye-icon (for Register Form) *********************************/
  function show_hide_password_function_register(){
-    const password_input   = document.querySelector("#password");
-    const dot_eye_password = document.querySelector("#dot-eye-icon-password");
+    const password_input = document.querySelector("#password");
+    const dot_eye        = document.querySelector("#dot-eye-icon-password");
 
     if(password_input.getAttribute('type') === "password"){
         password_input.setAttribute('type', 'text'); //also => password_input.type = "text"; (but not preferred!)
-        if(dot_eye_password.classList.contains("fa-eye")){
-            dot_eye_password.classList.remove("fa-eye");
+        if(dot_eye.classList.contains("fa-eye")){
+            dot_eye.classList.remove("fa-eye");
         }
-        dot_eye_password.classList.add("fa-eye-slash");
-        dot_eye_password.style.color = "grey";
+        dot_eye.classList.add("fa-eye-slash");
+        dot_eye.style.color = "grey";
     } 
     else{
         password_input.setAttribute('type', 'password'); //also => password_input.type = "password"; (but not preferred!)
-        if(dot_eye_password.classList.contains("fa-eye-slash")){
-            dot_eye_password.classList.remove("fa-eye-slash");
+        if(dot_eye.classList.contains("fa-eye-slash")){
+            dot_eye.classList.remove("fa-eye-slash");
         }
-        dot_eye_password.classList.add("fa-eye");
-        dot_eye_password.style.color = "inherit";
+        dot_eye.classList.add("fa-eye");
+        dot_eye.style.color = "inherit";
     }
 }
 
 function show_hide_confirm_password_function_register(){
-    const confirm_password_input   = document.querySelector("#password-confirm");
-    const dot_eye_confirm_password = document.querySelector("#dot-eye-icon-confirm-password");
+    const confirm_password_input = document.querySelector("#password-confirm");
+    const dot_eye                = document.querySelector("#dot-eye-icon-confirm-password");
 
     if(confirm_password_input.getAttribute('type') === "password"){
         confirm_password_input.setAttribute('type', 'text'); //also => password_input.type = "text"; (but not preferred!)
-        if(dot_eye_confirm_password.classList.contains("fa-eye")){
-            dot_eye_confirm_password.classList.remove("fa-eye");
+        if(dot_eye.classList.contains("fa-eye")){
+            dot_eye.classList.remove("fa-eye");
         }
-        dot_eye_confirm_password.classList.add("fa-eye-slash");
-        dot_eye_confirm_password.style.color = "grey";
+        dot_eye.classList.add("fa-eye-slash");
+        dot_eye.style.color = "grey";
     } 
     else{
         confirm_password_input.setAttribute('type', 'password'); //also => password_input.type = "password"; (but not preferred!)
-        if(dot_eye_confirm_password.classList.contains("fa-eye-slash")){
-            dot_eye_confirm_password.classList.remove("fa-eye-slash");
+        if(dot_eye.classList.contains("fa-eye-slash")){
+            dot_eye.classList.remove("fa-eye-slash");
         }
-        dot_eye_confirm_password.classList.add("fa-eye");
-        dot_eye_confirm_password.style.color = "inherit";
+        dot_eye.classList.add("fa-eye");
+        dot_eye.style.color = "inherit";
     }
 }
  /********************************* END show/hide password dot-eye-icon (for Register Form) *********************************/  
